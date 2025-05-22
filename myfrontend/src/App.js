3309a0526038f4c6c4ca48b30ab1ceb6e6d55d05
ayush17112005 <saxenaayushman99@gmail.com>
2025-05-22 23:21:12 +0530

    Add newfiles and remove unused files

---
+++ myfrontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SentimentAnalyzer from "./Sentiment";
+
+function App() {
+  return (
+    <div className="App">
+      <SentimentAnalyzer />
+    </div>
+  );
+}
+
+export default App;
